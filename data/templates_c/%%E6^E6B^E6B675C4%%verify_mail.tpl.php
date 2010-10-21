658613b4497657d88ccd4c05e44c519ae1a85262
Lukas Martini <lukas.martini@junge-piraten.de>
2010-10-21 13:39:14 +0200

    Ignore compiled templates

--- data/templates_c/%%E6^E6B^E6B675C4%%verify_mail.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2010-09-01 13:01:50
-         compiled from verify_mail.tpl */ ?>
-<p>Um Ihre E-Mail Adresse zu verifizieren, wird eine Mail an <?php echo $this->_tpl_vars['mail']; ?>
- geschickt, die einen Best&auml;tigungslink enth&auml;lt. Klicken Sie auf diesen Link, um die Verifizierung abzuschlie&szlig;en.</p>
-<form action="?do=verify_mail" class="verify_mail" method="post">
-	<input type="hidden" name="mail" value="<?php echo $this->_tpl_vars['mail']; ?>
-" />
-	<input class="submit" type="submit" name="send" value="Best&auml;tigungsmail senden" />
-</form>
