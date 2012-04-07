2cf350cef2de2558642f961303e6b5fbac16f355
Patrick Rauscher <prauscher@ohai.su>
2012-04-07 00:12:12 +0000

    startseite gefixt

---
+++ data/templates/home.tpl
@@ -0,0 +1,12 @@
+{include file="header.tpl"}
+<p class="greeting">Willkommen, {$user}!</p>";
+
+{if $showAddMail}
+<p class="note">Deinem Account sind noch keine Mailadressen zugeordnet. Um die Funktionen dieses Panels voll nutzen zu k&ouml;nnen, solltest du <a href="?module=profile&amp;do=add_mail">eine Mailadresse eintragen</a>.</p>
+{elseif $showVerify}
+<p class="note">Deine Mailadresse wurde bisher noch nicht verifiziert. Ohne Verifizierung stehen einige Funktionen (wie das vereinfachte Abonnieren von Mailinglisten) nicht zur 
+Verf&uuml;gung. <a href="?module=profile&amp;do=verify_mail&amp;mail=" . array_shift($user->getMails()) . "">Jetzt verifizieren</a>.</p>
+{else}
+<p class="note">Dein Account ist verifiziert - Es kann los gehen :).</p>
+{/if}
+{include file="footer.tpl"}
--- mod/home.mod.php
+++ mod/home.mod.php
@@ -8,24 +8,15 @@ class home {
 	}
 
 	public function main() {
-		global $user;
-
-		ob_start();
-
-		echo "<p class=\"greeting\">Willkommen, " . $user->getUid() . "!</p>";
+		global $user, $smarty;
 
 		if (!$user->hasMail()) {
-			echo "<p class=\"note\">Deinem Account sind noch keine Mailadressen zugeordnet. Um die Funktionen dieses Panels voll nutzen zu k&ouml;nnen, solltest du <a href=\"?module=profile&amp;do=add_mail\">eine Mailadresse eintragen</a>.</p>";
+			$smarty->assign("showAddMail", 1);
 		} else if (!$user->isVerified()) {
-			echo "<p class=\"note\">Deine Mailadresse wurde bisher noch nicht verifiziert. Ohne Verifizierung stehen einige Funktionen (wie das vereinfachte Abonnieren von Mailinglisten) nicht zur Verf&uuml;gung. <a href=\"?module=profile&amp;do=verify_mail&amp;mail=" . array_shift($user->getMails()) . "\">Jetzt verifizieren</a>.</p>";
-		} else {
-			echo "<p class=\"note\">Dein Account ist verifiziert - Es kann los gehen :).</p>";
+			$smarty->assign("showVerify", 1);
 		}
 
-		$output = ob_get_contents();
-		ob_end_clean();
-
-		return $output;
+		return $smarty->fetch("home.tpl");
 	}
 }
 
